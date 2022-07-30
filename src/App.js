3e39910a4ca825ee54273892896bf37eaeeac69d
epark272 <epark272@users.noreply.github.com>
2022-07-29 22:02:15 -0700

    added basic info

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This website is still under development. Go back to{" "}
+          <a
+            className="App-link"
+            href="https://edwardpark.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            my personal site
+          </a>
+          , or go to the{" "}
+          <a
+            className="App-link"
+            href="https://inst.eecs.berkeley.edu/~cs61b/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            official CS 61B course website
+          </a>
+          .
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
